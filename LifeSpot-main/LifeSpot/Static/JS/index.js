fcb4e043bb18113f4756f918f130e829a7ba3cb9
valmon55 <fedor_ka@mail.ru>
2023-12-20 17:10:21 +0300

    30.1.12

--- LifeSpot-main/LifeSpot/Static/JS/index.js
+++ LifeSpot-main/LifeSpot/Static/JS/index.js
@@ -5,31 +5,53 @@
 * */
 //let session =  new Map();
 //используем объект
-let session = {
-    "startDate": new Date().toLocaleString(),
-    "userAgent": window.navigator.userAgent,
-    "userAge": prompt("Пожалуйста, введите ваш возраст?")
-};
-
+//let session = {
+//    "startDate": new Date().toLocaleString(),
+//    "userAgent": window.navigator.userAgent,
+//    "userAge": prompt("Пожалуйста, введите ваш возраст?")
+//};
+//sessionStorage.setItem("startDate", new Date().toLocaleString());
+//sessionStorage.setItem("userAgent", window.navigator.userAgent);
+//sessionStorage.setItem("userAge", prompt("Пожалуйста, введите ваш возраст?"));
 /*
 * Сохранение данных сессии сразу при заходе пользователя на страницу
 * 
 * */
-function handleSession(){
+function handleSession(logger, checker){
     // Сохраним время начала сессии
-    session.set("startDate", new Date().toLocaleString())
-    // Сохраним UserAgent
-    session.set("userAgent", window.navigator.userAgent)
+    if (window.sessionStorage.getItem("startDate") == null) {
+        window.sessionStorage.setItem("startDate", new Date().toLocaleString())
+    }
+    if (window.sessionStorage.getItem("userAgent") == null) {
+        window.sessionStorage.setItem("userAgent", window.navigator.userAgent)
+    }
+    if (window.sessionStorage.getItem("userAge") == null) {
+        let age = prompt("Пожалуйста, введите ваш возраст ?");
+        window.sessionStorage.setItem("userAge", age);
+
+        /* Возраст отсутствовал в sessionStorage. Значит, это первый визит пользователя, и
+        при прохождении проверки на возраст он увидит приветствие*/
+        checker(true)
+    } else {
+        /* Пользователь заходит не первый раз, приветствие не показываем. */
+        checker(false)
+    }
+    /* Вызываем переданную в качестве колл-бэка функцию логирования.
+        передавать в качестве коллбека не обязательно, можно вызвать и напрямую, но мы добавили для повторения.
+    */
+    logger()
 }
 
 /*
 * Проверка возраста пользователя
 * 
 * */
-function checkAge(){
-    
-    if (session["userAge"] >= 18){
-        alert("Приветствуем на LifeSpot! " + '\n' +  "Текущее время: " + new Date().toLocaleString() );
+let checker = function ( newVisit ){
+
+    if (window.sessionStorage.getItem("userAge") >= 18) {
+        if (newVisit) {
+            alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + new Date().toLocaleString());
+        }
     }
     else{
         alert("Наши трансляции не предназначены для лиц моложе 18 лет. ВыL будете перенаправлены");
@@ -42,10 +64,10 @@ function checkAge(){
 * Вывод данных сессии в консоль
 * 
 * */
-let sessionLog = function () {
-    console.log('Начало сессии: ' + session.startDate);
-    console.log('Данные клиента: ' + session.userAgent);
-    console.log('Возраст пользователя: : ' + session.userAge);
+let logger = function () {
+    console.log('Начало сессии: ' + window.sessionStorage.getItem("startDate") );
+    console.log('Данные клиента: ' + window.sessionStorage.getItem("userAgent") );
+    console.log('Возраст пользователя: : ' + window.sessionStorage.getItem("userAge") );
 }
 
 /*
@@ -56,8 +78,8 @@ let sessionLog = function () {
 
 function filterContent(){
     let elements = document.getElementsByClassName('video-container');
-
-    for (let i = 0; i <= elements.length; i++ ){
+    debugger;
+    for (let i = 0; i < elements.length; i++ ){
         let videoText = elements[i].getElementsByTagName('h3')[0].innerText;
 
         if(!videoText.toLowerCase().includes(inputParseFunction().toLowerCase())){
