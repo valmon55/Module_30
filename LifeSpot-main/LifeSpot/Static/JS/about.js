496cb09dc56cf086158c147b2537e900d73cbd2e
valmon55 <fedor_ka@mail.ru>
2023-12-21 10:50:18 +0300

    30.2.4

--- LifeSpot-main/LifeSpot/Static/JS/about.js
+++ LifeSpot-main/LifeSpot/Static/JS/about.js
@@ -3,27 +3,34 @@
 * и сохраним отзыв в объект
 * 
 * */
-function getReview() {
+function getComment() {
     // Создадим объект
-    let review = {}
+    let comment = {};
     
     // Сохраним свойство имени
-    review["userName"] = prompt("Как вас зовут ?")
-    if(review["userName"] == null){
+    comment.author = prompt("Как вас зовут ?")
+    if (comment.author == null){
         return
     }
     
     // Сохраним текст отзыва
-    review["comment"] = prompt("Напишите свой отзыв")
-    if(review["comment"] == null){
+    comment.text = prompt("Напишите свой отзыв")
+    if (comment.text == null) {
         return
     }
     
     // Сохраним текущее время
-    review["date"] = new Date().toLocaleString()
-    
-    // Добавим на страницу
-    writeReview(review)
+    comment.date = new Date().toLocaleString();
+    let enableLikes = confirm('Разрешить пользователям оценивать ваш отзыв?')
+
+    if (enableLikes) {
+        review = Object.create(comment);
+        review.rate = 0;
+        writeReview(review)
+    }
+    else {
+        writeReview(comment)
+    }
 }
 
 /*
@@ -31,8 +38,14 @@ function getReview() {
 * 
 * */
 const writeReview = review => {
+    let likeCounter = '';
+
+    // Для проверки, является ли объект отзывом, используем свойство hasOwnProperty
+    if (review.hasOwnProperty('rate')) {
+        likeCounter += '           <b style="color: chocolate">Рейтинг:</b>   ' + review.rate;
+    }
     document.getElementsByClassName('reviews')[0].innerHTML += '    <div class="review-text">\n' +
-        `<p> <i> <b>${review['userName']}</b>  ${review['date']}</i></p>` +
-        `<p>${review['comment']}</p>`  +
+        `<p> <i> <b>${review['author']}</b>  ${review['date']}${likeCounter}</i></p>` +
+        `<p>${review['text']}</p>`  + 
         '</div>';
 }
