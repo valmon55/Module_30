f8aae859658d70d0d39be2ea6d9655d80cd83676
valmon55 <fedor_ka@mail.ru>
2023-12-21 17:27:02 +0300

    30.3.7

--- LifeSpot-main/LifeSpot/Static/JS/about.js
+++ LifeSpot-main/LifeSpot/Static/JS/about.js
@@ -48,6 +48,26 @@ function addComment() {
         writeReview(comment)
     }
 }
+function addLike(id) {
+    // Найдём нужный элемент по id
+    let element = document.getElementById(id);
+
+    // Преобразуем текст элемента в массив, разбив его по пробелам (так как счётчик лайков у нас отделен от символа ❤️пробелом)
+    let array = element.innerText.split(' ')
+
+    // Вытащим искомое значение счётчика и сразу же преобразуем его в число, так как
+    // при сложении любого значения со строкой в JS будет строка, а нам этого не требуется
+    let resultNum = parseInt(array[array.length - 1], 10);
+
+    // Увеличим счётчик
+    resultNum += 1
+
+    // Сохраним измененное значение обратно в массив
+    array[array.length - 1] = `${resultNum}`
+
+    // Обновим текст элемента
+    element.innerText = array.join(' ')
+}
 /*
 * Запишем отзыв на страницу 
 * 
@@ -55,12 +75,19 @@ function addComment() {
 const writeReview = review => {
     let likeCounter = '';
 
-    // Для проверки, является ли объект отзывом, используем свойство hasOwnProperty
+    // Если публикуется отзыв - добавляем ему кнопку с лайками.
     if (review.hasOwnProperty('rate')) {
-        likeCounter += '           <b style="color: chocolate">Рейтинг:</b>   ' + review.rate;
+
+        // Генерим идентификатор комментария.
+        let commentId = Math.random();
+        // Для кнопки лайков добавляем: идентификатор, атрибут onclick для передачи идентификатора в функцию, значок лайка, и само значение счётчика отделяем пробелом
+        // Также мы добавили стиль, чтобы кнопка смотрелась лучше и не имела рамок
+        likeCounter += '<button id="' + commentId +
+            '" style="border: none" onclick="addLike(this.id)">' +
+            `❤️ ${review.rate}</button>`
     }
-    document.getElementsByClassName('reviews')[0].innerHTML += '    <div class="review-text">\n' +
-        `<p> <i> <b>${review['author']}</b>  ${review['date']}${likeCounter}</i></p>` +
-        `<p>${review['text']}</p>`  + 
-        '</div>';
+    // Запишем результат 
+    document.getElementsByClassName('reviews')[0].innerHTML += ' <div class="review-    text">\n' +
+        `<p> <i> <b>${review['author']}</b> ${review['date']}${likeCounter}</i></p>` +
+        `<p>${review['text']}</p>` + '</div>';
 }
